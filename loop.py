046374b11a3201498146940c86935db1804d5997
ItzAtharva <athrvajagtap000@gmail.com>
2024-02-28 15:44:37 +0530

    string reverse without using inbuilt funcution

--- loop.py
+++ loop.py
@@ -1,3 +1,2 @@
 for num in range(1, 50):
-    
     print(num)
---
+++ string_reverse_program.py
@@ -0,0 +1,7 @@
+s=str(input("Enter your string :- "))
+output=''
+i=len(s)-1
+while i>=0:
+    output=output+s[i]
+    i=i-1
+    print(output)
