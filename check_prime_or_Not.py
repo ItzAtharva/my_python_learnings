fdb851517cbddbb1cf368d7808f33b100c82b5b0
ItzAtharva <athrvajagtap000@gmail.com>
2024-02-29 14:15:17 +0530

    prime no program updated

--- check_prime_or_Not.py
+++ check_prime_or_Not.py
@@ -1,4 +1,11 @@
-no =int(input("enter your Numebr :- "))
-if no==1:
-    print("it is not a prime number ")
-if
+number = int(input("Enter any number: "))
+
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(number, "is not a prime number")
+            break
+    else:
+        print(number, "is a prime number")
+else:
+    print(number, "is not a prime number")
